aeb1aec75208bc1ce5898cf200ba995c95c55a6b
LagunaJS <jhonlpdev@gmail.com>
2016-06-24 16:27:18 -0700

    Component Router

--- app/ps-movies/module.js
+++ app/ps-movies/module.js
@@ -1,13 +1,12 @@
 (function () {
     "use strict";
 
-    var module = angular.module("psMovies", ["ngRoute"]);
+    var module = angular.module("psMovies", ["ngComponentRouter"]);
     
-    module.config(function ($routeProvider) {
-        $routeProvider
-            .when("/list", { template: "<movie-list></movie-list>" })
-            .when("/about", { template: "pinche puto" })
-            .otherwise({ redirectTo: "/list" });
-    })
+    module.value("$routerRootComponent", "movieApp");
+
+    module.component("appAbout", {
+        template:"Pinche puto"
+    });
 
 }());
